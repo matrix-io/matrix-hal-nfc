27cf37892dd6fe1d889fe8b4ce006a4b423ea12a
Boris Ermakov-Spektor <b.ermakovspektor@ufl.edu>
2019-06-24 17:55:24 -0400

    Refactor to contain HAL vars in nfc_sensor.cpp

--- src/nfc_sensor.cpp
+++ src/nfc_sensor.cpp
@@ -6,44 +6,44 @@ using std::cerr;
 using std::cout;
 using std::endl;
 
-void ReadNdefMessage(phalTop_Sw_DataParams_t *phalTop, uint8_t topTagType,
-                     matrix_hal::NFCData *nfcData) {
-  // // Get the phalTop component
-  // phalTop_Sw_DataParams_t *pTop =
-  //     (phalTop_Sw_DataParams_t *)phNfcLib_GetDataParams(PH_COMP_AL_TOP);
-  // uint8_t NDEFbuffer[1024];
-  // phStatus_t status;
-  // uint8_t bTagState;
-  // uint16_t NDEFlength = 0;
+// void ReadNdefMessage(phalTop_Sw_DataParams_t *phalTop, uint8_t tagTechnology,
+//                      matrix_hal::NFCData *nfcData) {
+// // Get the phalTop component
+// phalTop_Sw_DataParams_t *pTop =
+//     (phalTop_Sw_DataParams_t *)phNfcLib_GetDataParams(PH_COMP_AL_TOP);
+// uint8_t NDEFbuffer[1024];
+// phStatus_t status;
+// uint8_t bTagState;
+// uint16_t NDEFlength = 0;
 
-  // /* Configure Top layer for specified tag type */
-  // status = phalTop_SetConfig(&pTop, PHAL_TOP_CONFIG_TAG_TYPE, topTagType);
-  // if (status != PH_ERR_SUCCESS) return;
+// /* Configure Top layer for specified tag type */
+// status = phalTop_SetConfig(&pTop, PHAL_TOP_CONFIG_TAG_TYPE, topTagType);
+// if (status != PH_ERR_SUCCESS) return;
 
-  // /* Check for NDEF presence */
-  // status = phalTop_CheckNdef(&pTop, &bTagState);
-  // if (status != PH_ERR_SUCCESS) return;
+// /* Check for NDEF presence */
+// status = phalTop_CheckNdef(&pTop, &bTagState);
+// if (status != PH_ERR_SUCCESS) return;
 
-  // if ((bTagState == PHAL_TOP_STATE_READONLY) ||
-  //     (bTagState == PHAL_TOP_STATE_READWRITE)) {
-  //   /* Read NDEF message */
-  //   status = phalTop_ReadNdef(&pTop, NDEFbuffer, &NDEFlength);
-  //   if (status != PH_ERR_SUCCESS) return;
+// if ((bTagState == PHAL_TOP_STATE_READONLY) ||
+//     (bTagState == PHAL_TOP_STATE_READWRITE)) {
+//   /* Read NDEF message */
+//   status = phalTop_ReadNdef(&pTop, NDEFbuffer, &NDEFlength);
+//   if (status != PH_ERR_SUCCESS) return;
 
-  //   if (NDEFlength > 0) {
-  //     // DEBUG_PRINTF("\tNDEF detected...\n");
-  //     // DEBUG_PRINTF("\tNDEF length: %d\n", lengthNDEF);
-  //     // DEBUG_PRINTF("\tNDEF message:\n");
-  //     // DumpBuffer(aData, lengthNDEF);
-  //     // DumpBuffer(bufferNDEF, 50);
-  //     nfcData->NDEF = std::unique_ptr<std::vector<uint8_t>>(
-  //         new std::vector<uint8_t>(NDEFbuffer, NDEFbuffer + NDEFlength));
-  //   } else {
-  //     nfcData->NDEF =
-  //         std::unique_ptr<std::vector<uint8_t>>(new std::vector<uint8_t>());
-  //   }
-  // }
-}
+//   if (NDEFlength > 0) {
+//     // DEBUG_PRINTF("\tNDEF detected...\n");
+//     // DEBUG_PRINTF("\tNDEF length: %d\n", lengthNDEF);
+//     // DEBUG_PRINTF("\tNDEF message:\n");
+//     // DumpBuffer(aData, lengthNDEF);
+//     // DumpBuffer(bufferNDEF, 50);
+//     nfcData->NDEF = std::unique_ptr<std::vector<uint8_t>>(
+//         new std::vector<uint8_t>(NDEFbuffer, NDEFbuffer + NDEFlength));
+//   } else {
+//     nfcData->NDEF =
+//         std::unique_ptr<std::vector<uint8_t>>(new std::vector<uint8_t>());
+//   }
+// }
+// }
 
 void ExportTagInfo(phacDiscLoop_Sw_DataParams_t *pDataParams,
                    uint16_t tagTechnology, matrix_hal::NFCData *nfcData) {
@@ -66,7 +66,7 @@ void ExportTagInfo(phacDiscLoop_Sw_DataParams_t *pDataParams,
     if (pDataParams->sTypeATargetInfo.bT1TFlag) {
       nfcData->type = "1";
       /* Check for NDEF and Read */
-      ReadNdefMessage(nullptr, PHAL_TOP_TAG_TYPE_T1T_TAG, nfcData);
+      // ReadNdefMessage(nullptr, PHAL_TOP_TAG_TYPE_T1T_TAG, nfcData);
     } else {
       bTagType = (pDataParams->sTypeATargetInfo.aTypeA_I3P3[0].aSak & 0x60);
       bTagType = bTagType >> 5;
@@ -74,12 +74,12 @@ void ExportTagInfo(phacDiscLoop_Sw_DataParams_t *pDataParams,
         case PHAC_DISCLOOP_TYPEA_TYPE2_TAG_CONFIG_MASK:
           nfcData->type = "2";
           /* Check for NDEF and Read */
-          ReadNdefMessage(nullptr, PHAL_TOP_TAG_TYPE_T2T_TAG, nfcData);
+          // ReadNdefMessage(nullptr, PHAL_TOP_TAG_TYPE_T2T_TAG, nfcData);
           break;
         case PHAC_DISCLOOP_TYPEA_TYPE4A_TAG_CONFIG_MASK:
           nfcData->type = "4A";
           /* Check for NDEF and Read */
-          ReadNdefMessage(nullptr, PHAL_TOP_TAG_TYPE_T4T_TAG, nfcData);
+          // ReadNdefMessage(nullptr, PHAL_TOP_TAG_TYPE_T4T_TAG, nfcData);
           break;
         case PHAC_DISCLOOP_TYPEA_TYPE_NFC_DEP_TAG_CONFIG_MASK:
           nfcData->type = "P2P";
@@ -87,7 +87,7 @@ void ExportTagInfo(phacDiscLoop_Sw_DataParams_t *pDataParams,
         case PHAC_DISCLOOP_TYPEA_TYPE_NFC_DEP_TYPE4A_TAG_CONFIG_MASK:
           nfcData->type = "NFC_DEP & 4A";
           /* Check for NDEF and Read */
-          ReadNdefMessage(nullptr, PHAL_TOP_TAG_TYPE_T4T_TAG, nfcData);
+          // ReadNdefMessage(nullptr, PHAL_TOP_TAG_TYPE_T4T_TAG, nfcData);
           break;
         default:
           break;
@@ -126,7 +126,7 @@ void ExportTagInfo(phacDiscLoop_Sw_DataParams_t *pDataParams,
     } else {
       nfcData->type = "3";
       /* Check for NDEF and Read */
-      ReadNdefMessage(nullptr, PHAL_TOP_TAG_TYPE_T3T_TAG, nfcData);
+      // ReadNdefMessage(nullptr, PHAL_TOP_TAG_TYPE_T3T_TAG, nfcData);
     }
     if (pDataParams->sTypeFTargetInfo.aTypeFTag[0].bBaud !=
         PHAC_DISCLOOP_CON_BITR_212) {
@@ -150,6 +150,8 @@ NFCSensor::NFCSensor() {
   if (nfcLibStatus != PH_NFCLIB_STATUS_SUCCESS) {
     throw "NFC Sensor Init Failed";
   }
+  pDiscLoop = (phacDiscLoop_Sw_DataParams_t *)phNfcLib_GetDataParams(
+      PH_COMP_AC_DISCLOOP);
 }
 
 NFCSensor::~NFCSensor() { (void)phNfcLib_DeInit(); }
@@ -157,7 +159,6 @@ NFCSensor::~NFCSensor() { (void)phNfcLib_DeInit(); }
 void NFCSensor::Read(NFCData *nfcData) {
   // Read function takes between 30ms and 40ms to run.
   nfcData->recentlyUpdated = false;
-  phStatus_t discLoopStatus = PHAC_DISCLOOP_LPCD_NO_TECH_DETECTED;
   uint16_t tagTechType = 0;
   // Disable blocking so that function will exit if no detection made
   phNfcLib_SetConfig_Value(PH_NFCLIB_CONFIG_ACTIVATION_BLOCKING, PH_OFF);
@@ -166,11 +167,8 @@ void NFCSensor::Read(NFCData *nfcData) {
     // cerr << "NFC Tag Activation Failed" << endl;
   }
   // Hook into the discovery loop and pull UID/AQT(A/B)/SAK/Type from there
-  phacDiscLoop_Sw_DataParams_t *pDiscLoop =
-      (phacDiscLoop_Sw_DataParams_t *)phNfcLib_GetDataParams(
-          PH_COMP_AC_DISCLOOP);
-  discLoopStatus = phacDiscLoop_GetConfig(
-      pDiscLoop, PHAC_DISCLOOP_CONFIG_TECH_DETECTED, &tagTechType);
+  phacDiscLoop_GetConfig(pDiscLoop, PHAC_DISCLOOP_CONFIG_TECH_DETECTED,
+                         &tagTechType);
   if (tagTechType != 0) ExportTagInfo(pDiscLoop, tagTechType, nfcData);
   nfcLibStatus =
       phNfcLib_Deactivate(PH_NFCLIB_DEACTIVATION_MODE_RELEASE, &PeerInfo);
--- src/nfc_sensor.h
+++ src/nfc_sensor.h
@@ -8,10 +8,36 @@
 
 #include "nfc_data.h"
 
-#include "nfc_abstraction.h"  // This MUST be included last!
+extern "C" {
+// This MUST be included last!
+#include <matrix_nfc/nxp_nfc/NxpNfcRdLib/intfs/phNfcLib.h>
+#include <matrix_nfc/nxp_nfc/NxpNfcRdLib/types/ph_Status.h>
 
-void ReadNdefMessage(phalTop_Sw_DataParams_t *phalTop, uint8_t tagTechnology,
-                     matrix_hal::NFCData *nfcData);
+// Begin Variable Setup for NXP HAL
+#define DATA_BUFFER_LEN 260            /* Buffer length */
+uint8_t bHalBufferTx[DATA_BUFFER_LEN]; /* HAL TX buffer. Size 260 - Based on
+                                          maximum FSL */
+uint8_t bHalBufferRx[DATA_BUFFER_LEN]; /* HAL RX buffer. Size 260 - Based on
+                                          maximum FSL */
+uint8_t sens_res[2] = {0x04, 0x00};    /* ATQ bytes - needed for anti-collision
+                                        */
+uint8_t nfc_id1[3] = {0xA1, 0xA2, 0xA3}; /* user defined bytes of the UID (one
+                                  is hardcoded)
+                                            - needed for anti-collision */
+uint8_t sel_res = 0x40;
+uint8_t nfc_id3 = 0xFA; /* NFC3 byte - required for anti-collision */
+uint8_t poll_res[18] = {0x01, 0xFE, 0xB2, 0xB3, 0xB4, 0xB5, 0xB6, 0xB7, 0xC0,
+                        0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0x23, 0x45};
+// Stubs, in case NXP HAL expects these implementations
+void pWtoxCallBck(uint8_t bTimerId) {}
+void pRtoxCallBck(uint8_t bTimerId) {}
+uint8_t aAppHCEBuf[32];
+uint16_t wAppHCEBuffSize = sizeof(aAppHCEBuf);
+// End Variable Setup for NXP HAL
+}
+
+// void ReadNdefMessage(phalTop_Sw_DataParams_t *phalTop, uint8_t tagTechnology,
+//                      matrix_hal::NFCData *nfcData);
 
 void ExportTagInfo(phacDiscLoop_Sw_DataParams_t *pDataParams,
                    uint16_t tagTechnology, matrix_hal::NFCData *nfcData);
@@ -30,7 +56,9 @@ class NFCSensor {
   phNfcLib_Transmit_t phNfcLib_TransmitInput;
   phNfcLib_PeerInfo_t PeerInfo = {0};
   uint8_t bDataBuffer[DATA_BUFFER_LEN]; /* universal data buffer */
-
+  // End Variable Setup for Simplified NXP Lib
+  // NXP Lib Component pointers
+  phacDiscLoop_Sw_DataParams_t *pDiscLoop;
   // Begin Public User Functions
   NFCSensor();
   ~NFCSensor();
