d5d65dfd6e368c39237e04a034a40fefcefc851d
Carlos Chacin <chacin.carlos9798@gmail.com>
2019-09-09 15:18:11 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
 #! /bin/bash
-mkdir -p build && cd build && cmake ../src && sudo make install -j4 && cd .. && sudo ldconfig && bash rebuild_examples.sh
+mkdir -p build
+cd build
+cmake ../src
+sudo make install -j4
+cd .. && sudo ldconfig
+bash rebuild_examples.sh
+printf "\n****************************\nMATRIX HAL NFC is Installed! \n****************************\n"
